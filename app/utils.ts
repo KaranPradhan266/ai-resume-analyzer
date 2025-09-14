09e46f1c23d83c922efe1581414d3facc1137b25
Karan Amal  Pradhan <kpradhan@Karans-MacBook-Pro.local>
2025-09-14 06:57:06 -0700

    Resume Feedback Components

--- app/utils.ts
+++ app/utils.ts
@@ -1,1 +1,8 @@
-export const generateUUId = () => crypto.randomUUID()
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
+
+export const generateUUId = () => crypto.randomUUID();
+
+export function cn(...inputs: ClassValue[]){
+    return twMerge(clsx(inputs))
+}
